b2a4141ee2b0236dcc397ef301a7f96d7597e934
Deepankar Mondal <deepankar.mondal@p360.com>
2023-03-16 13:27:53 -0400

    body parser

--- index.ts
+++ index.ts
@@ -1,13 +1,24 @@
 import express, { Express, Request , Response} from 'express';
 import dotenv from 'dotenv';
+import bodyparser from 'body-parser';
+
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
 
+app.use(bodyparser.urlencoded({extended:true}))
+app.use(bodyparser.json())
+
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
+});
+app.post('/', (req: Request, res: Response) => {
+  res.json({
+    data: req.body
+  })
+  //res.send('Express + TypeScript Server');
 });
 
 app.listen(port, () => {
